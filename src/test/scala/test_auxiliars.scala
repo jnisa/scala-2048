d76580e872b781aedb4e87021150c44c16aab668
joao.nisa <joao.nisa@primer.io>
2023-01-16 20:42:55 +0000

    fix the tests layer

--- src/test/scala/test_auxiliars.scala
+++ src/test/scala/test_auxiliars.scala
@@ -5,6 +5,7 @@ object ScalaTestingStyles
 
 import Main.game.AuxiliarFunctions.Auxiliars
 
+import scala.Tuple
 import scala.language.postfixOps
 import org.scalatest.funsuite.AnyFunSuite
 
@@ -13,14 +14,14 @@ class test_auxiliars extends AnyFunSuite:
   val empty_board = Array.fill(4, 4)(0)
 
   test("adjacent when the board is fully empty") {
-    assert(Auxiliars.get_adjacent_vals(empty_board, 1, 2) = ("0", "0", "0", "0"))
-    assert(Auxiliars.get_adjacent_vals(empty_board, 2, 2) = (0, 0, 0, 0))
+    assert(Auxiliars.get_adjacent_vals(empty_board, 1, 2) == (0, 0, 0, 0))
+    assert(Auxiliars.get_adjacent_vals(empty_board, 2, 2) == (0, 0, 0, 0))
   }
 
   test("when you hit the limits of the board") {
-    assert(Auxiliars.get_adjacent_vals(empty_board, 0, 0) = (None, 0, None, 0))
-    assert(Auxiliars.get_adjacent_vals(empty_board, 4, 4) = (0, None, 0, None))
-    assert(Auxiliars.get_adjacent_vals(empty_board, 5, 5) = (None, None, None, None))
+    assert(Auxiliars.get_adjacent_vals(empty_board, 0, 0) == (None, 0, None, 0))
+    assert(Auxiliars.get_adjacent_vals(empty_board, 4, 4) == (0, None, 0, None))
+    assert(Auxiliars.get_adjacent_vals(empty_board, 5, 5) == (None, None, None, None))
   }
 
 
