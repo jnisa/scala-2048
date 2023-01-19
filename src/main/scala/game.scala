48f86e009662273e883d5613ca651ec4f597a0e6
joao.nisa <joao.nisa@primer.io>
2023-01-19 00:23:37 +0000

    game working

--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -21,44 +21,47 @@ object GameLaunchpad {
     board(pos_1(0))(pos_1(1)) = 2
     board(pos_2(0))(pos_2(1)) = 2
 
-    draw_board(board)
+    while (true) {
+      draw_board(board)
 
-    //  while (true) {
-    //    println("Please enter the move (right, left, up, and down):")
-    //    val move = StdIn.readLine()
-    //
-    //    if (move == "right"){
-    //      val outcome = move_right(board, points)
-    //
-    //      board = outcome(0)
-    //      points = outcome(1)
-    //    }
-    //    else if (move == "left") {
-    //      val outcome = move_left(board, points)
-    //
-    //      board = outcome(0)
-    //      points = outcome
-    //    }
-    //    else if (move == "up") {
-    //      val outcome = move_up(board, points)
-    //
-    //      board = outcome(0)
-    //      points = outcome
-    //    }
-    //    else if (move == "down") {
-    //      val outcome = move_down(board, points)
-    //
-    //      board = outcome(0)
-    //      points = outcome
-    //    }
-    //    else {
-    //      println("The direction to move the board chosen is not supported!")
-    //    }
-    //
-    //    if (is_game_over(board)) {
-    //      println(f"Game over! Your score is $points%2.2f")
-    //      break
-    //    }
-    //  }
+      println("Please enter the move (right, left, up, and down):")
+      val move = StdIn.readLine()
+
+      if (move == "right"){
+        val outcome = move_right(board, points)
+
+        board = outcome.productElement(0).asInstanceOf[Array[Array[Int]]]
+        points = outcome.productElement(1).asInstanceOf[Int]
+      }
+      else if (move == "left") {
+        val outcome = move_left(board, points)
+
+        board = outcome.productElement(0).asInstanceOf[Array[Array[Int]]]
+        points = outcome.productElement(1).asInstanceOf[Int]
+      }
+      else if (move == "up") {
+        val outcome = move_up(board, points)
+
+        board = outcome.productElement(0).asInstanceOf[Array[Array[Int]]]
+        points = outcome.productElement(1).asInstanceOf[Int]
+      }
+      else if (move == "down") {
+        val outcome = move_down(board, points)
+
+        board = outcome.productElement(0).asInstanceOf[Array[Array[Int]]]
+        points = outcome.productElement(1).asInstanceOf[Int]
+      }
+      else {
+        println("The direction to move the board chosen is not supported!")
+      }
+
+      if (is_game_over(board)) {
+        println(f"Game over! Your score is $points%2.2f")
+        break
+      }
+
+      val new_pos = generate_board_position(board)
+      board(new_pos(0))(new_pos(1)) = 2
+    }
   }
 }
